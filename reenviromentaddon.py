c6d2cca8a9d6bea9a7e009b4e1a7a97ad70ad9cb
tilieve <li31838871@gmail.com>
2019-04-28 20:18:44 -0500

    Add files via upload

---
+++ reenviromentaddon.py
@@ -0,0 +1,6 @@
+class Env(dict):
+    def __init__(self, parms=(), args=(), outer=None):
+        self.update(zip(parms, args))
+        self.outer = outer
+    def find(self, var):
+        return self if (var in self) else self.outer.find(var)
