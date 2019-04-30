4d56d200f53096d8d99a870291b4f4296cb82c1d
Bender <isaac.littler@gmail.com>
2019-04-30 01:10:54 -0500

    fixed recent eval problem, set! likely doesnt work

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ class Env(dict):
         self.update(zip(parms, args))
         self.outer = outer
     def find(self, var):
-    return self if (var in self) else self.outer.find(var)
+        return self if (var in self) else self.outer.find(var)
 
 def standard_env():
     env = {}
@@ -62,44 +62,43 @@ def tokenize(source): #source: str
 
 counter = 0
 def read_from_tokens(tokens): #tokens: list
-        token = tokens.pop(0)
-        if token == '(':
-                tree = []
-                global counter
-                counter += 1
-                while tokens[0] != ')':
-                        temp = read_from_tokens(tokens)
-                        if temp is not None:
-                                tree.append(temp)
-                        if counter<0:
-                                raise SyntaxError('incorrect parenthesis')
-                tokens.pop(0) #removes ')'
-                counter -= 1
-                if (counter > 0):
-                        return tree #tree: list
-                else:
-                        if len(tokens) == 0 or tokens.pop(0) == ';':
-                                return tree
-                        else:
-                                raise SyntaxError('extra stuff after last parenthesis')
-                                
-        elif token == ')':
-                raise SyntaxError('you forgot a ( for your )')
-        elif token == ';':
-                return tree
-        elif token == '#|':
-                while len(tokens) > 0 and tokens[0] != '|#':
-                        tokens.pop(0)
-                if(len(tokens) == 0):
-                        return -1
-                else:
-                        tokens.pop(0) #removes '|#'
-                        if len(tokens) > 0:
-                                return read_from_tokens(tokens)
-                        else:
-                                return []
+    token = tokens.pop(0)
+    if token == '(':
+        tree = []
+        global counter
+        counter += 1
+        while tokens[0] != ')':
+            temp = read_from_tokens(tokens)
+            if temp is not None:
+                tree.append(temp)
+            if counter<0:
+                raise SyntaxError('incorrect parenthesis')
+        tokens.pop(0) #removes ')'
+        counter -= 1
+        if (counter > 0):
+            return tree #tree: list
         else:
-                return atom(token)
+            if len(tokens) == 0 or tokens.pop(0) == ';':
+                return tree
+            else:
+                raise SyntaxError('extra stuff after last parenthesis')                 
+    elif token == ')':
+        raise SyntaxError('you forgot a ( for your )')
+    elif token == ';':
+        return tree
+    elif token == '#|':
+        while len(tokens) > 0 and tokens[0] != '|#':
+            tokens.pop(0)
+        if(len(tokens) == 0):
+            return -1
+        else:
+            tokens.pop(0) #removes '|#'
+            if len(tokens) > 0:
+                return read_from_tokens(tokens)
+            else:
+                return []
+    else:
+        return atom(token)
 
 def atom(token): #token is a string
     #Numbers are read as numbers, everything else is a symbol for now"
@@ -111,16 +110,16 @@ def atom(token): #token is a string
                 
 #parses the program
 def parse(code):
-        L = tokenize(code)
-        if len(L) == 0:
-                return []
-              #  OR raise SyntaxError('write some code')
-        elif L[0] == ';':
-               return [];
-        elif L[0] != '(' and L[0] != '#|':
-                raise SyntaxError('wrong start')
-        else:
-            return read_from_tokens(tokenize(code))
+    L = tokenize(code)
+    if len(L) == 0:
+        return []
+    #  OR raise SyntaxError('write some code')
+    elif L[0] == ';':
+        return [];
+    elif L[0] != '(' and L[0] != '#|':
+        raise SyntaxError('wrong start')
+    else:
+        return read_from_tokens(tokenize(code))
 
 def run(filename):
     code = read_from_file(filename)
@@ -129,39 +128,39 @@ def run(filename):
             print(eval(code[i]))
 
 def read_from_file(filename):
-        flag = False
-        L = []
-        with open(filename) as f:
-                for line in f:
-                      if(flag):
-                               s = line
-                               test = tokenize(s)
-                               try:
-                                    index = test.index('|#')
-                               except ValueError:  
-                                    index = -1
-                               if(index != -1):
-                                        while test[0] != '|#':
-                                             test.pop(0)
-                                        test.pop(0)
-                                        test = "".join(test)
-                                        temp =  parse(test)
-                                        flag = False
-                               else:
-                                        continue
-                      else:
-                              temp =  parse(line)
-                      if(temp == -1):
-                              flag = True
-                              continue
-                      L.append(temp) #return
-                if(flag):
-                      raise SyntaxError('you forgot to end the comment')
-        return L
+    flag = False
+    L = []
+    with open(filename) as f:
+        for line in f:
+            if(flag):
+                s = line
+                test = tokenize(s)
+                try:
+                    index = test.index('|#')
+                except ValueError:  
+                     index = -1
+                if(index != -1):
+                    while test[0] != '|#':
+                        test.pop(0)
+                    test.pop(0)
+                    test = "".join(test)
+                    temp =  parse(test)
+                    flag = False
+                else:
+                    continue
+            else:
+                temp =  parse(line)
+            if(temp == -1):
+                flag = True
+                continue
+            L.append(temp) #return
+        if(flag):
+            raise SyntaxError('you forgot to end the comment')
+    return L
 
 def eval(x, env = global_env):
     if isinstance(x, Symbol):   
-        return env.find(x)[x]
+        return env[x] #changed from env.find(x)[x] due to errors
     elif not isinstance(x, List):
         return x   
     op, *args = x       
@@ -185,14 +184,14 @@ def eval(x, env = global_env):
         vals = [eval(arg, env) for arg in args]
         return proc(*vals)
 
-    def repl(prompt='lis.py> '):
-        while True:
-            val = eval(parse(raw_input(prompt)))
-            if val is not None: 
-                print(schemestr(val))
+def repl(prompt='RIZ> '):
+    while True:
+        val = eval(parse(input(prompt)))
+        if val is not None: 
+            print(schemestr(val))
 
-    def schemestr(exp):
-        if isinstance(exp, List):
-            return '(' + ' '.join(map(schemestr, exp)) + ')' 
-        else:
-            return str(exp)
+def schemestr(exp):
+    if isinstance(exp, List):
+        return '(' + ' '.join(map(schemestr, exp)) + ')' 
+    else:
+        return str(exp)
